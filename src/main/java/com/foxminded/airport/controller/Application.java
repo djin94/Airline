3f2951c19debb96bd8139bfd8b008125740eee40
EvgenyK <djin94@yandex.ru>
2018-09-26 22:00:01 +0300

    Update

---
+++ src/main/java/com/foxminded/airport/controller/Application.java
@@ -0,0 +1,4 @@
+package com.foxminded.airport.controller;
+
+public class Application {
+}
---
+++ src/main/java/com/foxminded/airport/dao/Storage.java
@@ -0,0 +1,15 @@
+package com.foxminded.airport.dao;
+
+import java.util.List;
+
+public interface Storage<T> {
+    void save(T t);
+
+    void update(T t);
+
+    void delete(T t);
+
+    T findById(int id);
+
+    List<T> getAll();
+}
