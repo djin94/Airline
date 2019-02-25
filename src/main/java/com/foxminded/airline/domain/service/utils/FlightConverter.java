6058d7209c90acd0103666a4c3e355ee823d223e
EvgenyK <djin94@yandex.ru>
2019-02-25 14:01:12 +0300

    Merge branch 'dev' of https://github.com/djin94/Airline into dev
    # Conflicts:
    #	src/test/java/com/foxminded/airline/web/controller/AdminControllerSmokeTest.java

--- src/main/java/com/foxminded/airline/domain/service/utils/FlightConverter.java
+++ src/main/java/com/foxminded/airline/domain/service/utils/FlightConverter.java
@@ -1,7 +1,9 @@
 package com.foxminded.airline.domain.service.utils;
 
 import com.foxminded.airline.domain.entity.Flight;
+import com.foxminded.airline.domain.service.FlightService;
 import com.foxminded.airline.web.dto.FlightDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -11,6 +13,10 @@ import java.util.List;
 
 @Component
 public class FlightConverter {
+
+    @Autowired
+    private FlightService flightService;
+
     public List<FlightDTO> createDTOsForFlights(List<Flight> flights) {
         List<FlightDTO> flightDTOS = new ArrayList<>();
         flights.forEach(flight -> {
@@ -28,7 +34,7 @@ public class FlightConverter {
         flightDTO.setTimeString(flight.getTime().toString());
         flightDTO.setDepartureAirport(flight.getDepartureAirport().getName());
         flightDTO.setArrivalAirport(flight.getArrivalAirport().getName());
-        flightDTO.setEnabled(isFlightEnabled(flight));
+        flightDTO.setEnabled(flightService.isFlightEnabled(flight));
         return flightDTO;
     }
 }
